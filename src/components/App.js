9a7e91dc48ddd02278603322716867710cb7ebb8
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-22 14:18:37 +0200

    add create-my-react-app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>Holi Mundi</ul>
+    </div>
+  );
+}
+
+export default App;
