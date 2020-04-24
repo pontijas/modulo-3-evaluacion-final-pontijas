8aac07ad7a4e1f11a90100a6a9618d4672f8f92c
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-24 12:41:45 +0200

    customize character route with id

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,13 +2,17 @@ import React from 'react';
 import '../stylesheets/App.scss';
 
 const Character = (props) => {
+  // console.log('recibo esto en caracter', props.character.id);
+
   return (
     <section className="card__container">
-      <img src={props.character.image} alt={`Imagen de ${props.character.name}`} title={`Imagen de ${props.character.name}`} width="160px"></img>
-      <small className="card__info">
-        <h2>{props.character.name}</h2>
-        <h3>{props.character.species}</h3>
-      </small>
+      <a href={`#/character/${props.character.id}`}>
+        <img src={props.character.image} alt={`Imagen de ${props.character.name}`} title={`Imagen de ${props.character.name}`} width="160px"></img>
+        <small className="card__info">
+          <h2>{props.character.name}</h2>
+          <h3>{props.character.species}</h3>
+        </small>
+      </a>
     </section>
   );
 };
