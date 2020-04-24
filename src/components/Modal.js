af1d91009ac73ddf7b1b60876201988404573e77
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-24 13:43:39 +0200

    set modal items

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,15 @@ import React from 'react';
 import '../stylesheets/App.scss';
 
 const Modal = () => {
-  return <section className="modal__container">modal</section>;
+  return (
+    <section className="modal__container">
+      <img src="https://via.placeholder.com/200" alt="Character image" title="Character image" />
+      <section className="modal__info">
+        <h2>Name</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eius quod commodi perspiciatis architecto nisi voluptas tempore, odio perferendis doloremque, quas illo inventore aspernatur corporis, alias expedita. Odio, exercitationem esse?</p>
+      </section>
+    </section>
+  );
 };
 
 export default Modal;
